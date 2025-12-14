8a1593ec7ef86628d9fc7205f48e3555996a2671
AkingbadeOmosebi <akingbadeomosebi@gmail.com>
2025-12-14 15:45:31 +0100

    oidc-auth

---
+++ terraform/oidc/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+  # Uses az login credentials automatically
+}
+
+# Get current user/client information
+data "azurerm_client_config" "current" {}
+
+# Create Azure AD Application
+resource "azuread_application" "github_oidc" {
+  display_name = "GitHub-OIDC-3Tier-MERN-App"
+  owners       = [data.azurerm_client_config.current.object_id]
+}
+
+# Create Service Principal for the Application
+resource "azuread_service_principal" "github_oidc" {
+  client_id = azuread_application.github_oidc.client_id
+  owners    = [data.azurerm_client_config.current.object_id]
+}
+
+# Create Federated Identity Credential for GitHub Actions
+resource "azuread_application_federated_identity_credential" "github_actions" {
+  application_id = azuread_application.github_oidc.id
+  display_name   = "GitHub-Actions-MERN-App"
+  description    = "OIDC for GitHub Actions in 3-Tier-MERN-App repository"
+
+  audiences = ["api://AzureADTokenExchange"]
+  issuer    = "https://token.actions.githubusercontent.com"
+  subject   = "repo:AkingbadeOmosebi/3-Tier-MERN-App:ref:refs/heads/main"
+}
+
+# Create Resource Group for AKS and related resources
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "rg-3tier-mern-aks"
+  location = "West Europe"
+}
+
+# Assign Contributor role to the Service Principal for the Resource Group
+resource "azurerm_role_assignment" "github_oidc_contributor" {
+  scope                = azurerm_resource_group.aks_rg.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.github_oidc.object_id
+}
